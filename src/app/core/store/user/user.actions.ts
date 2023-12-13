947fea4fc5fdf3242e5f0669e40d8b8c270f0e71
Tarik Omeragic <tarik.omeragic@olympia-technology.com>
2023-12-13 21:16:19 +0100

    Ngrx cleanup

--- src/app/core/store/user/user.actions.ts
+++ src/app/core/store/user/user.actions.ts
@@ -6,10 +6,6 @@ export const FETCH_USERS = '[Users] Fetch users';
 export const FETCH_USERS_SUCCESS = '[Users] Fetch users success';
 export const FETCH_USERS_ERROR = '[Users] Fetch users error';
 
-export const FETCH_USER_BY_ID = '[Users] Fetch user by id';
-export const FETCH_USER_BY_ID_SUCCESS = '[Users] Fetch user by id success';
-export const FETCH_USER_BY_ID_ERROR = '[Users] Fetch user by id error';
-
 export const fetchUsers = createAction(
     FETCH_USERS,
 );
